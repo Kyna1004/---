a4e52ae6806d972b25d57b01ae6f50e5930a7d0b
Kyna1004 <zengkeyi1004@gmail.com>
2025-12-12 14:59:20 +0800

    app.py

--- app.py
+++ app.py
@@ -181,11 +181,7 @@ def calc_metrics_dict(df_chunk):
     res['cpm'] = (sums['spend'] / (sums['impressions'] + eps)) * 1000
     res['cpc'] = sums['spend'] / (sums['clicks'] + eps)
     res['ctr'] = sums['clicks'] / (sums['impressions'] + eps)
-    def calculate_cpa(spend, purchases):
-    if purchases > 0:
-        return spend / purchases
-    else:
-        return 0.0 
+    res['cpa'] = sums['spend'] / (sums['purchases'] + eps) 
     res['cvr_purchase'] = sums['purchases'] / (sums['clicks'] + eps)
     res['rate_click_to_lp'] = sums['landing_page_views'] / (sums['clicks'] + eps)
     res['rate_lp_to_atc'] = sums['add_to_cart'] / (sums['landing_page_views'] + eps)
